128f28a0d06bff9d88789257fa353b1641ce6998
KavindaGimhan <kavinda_d@epiclanka.net>
2026-01-21 16:14:35 +0530

    cache bust

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -19,6 +19,11 @@ axiosInstance.interceptors.request.use(
       config.headers['X-Tenant-Id'] = tenantId;
     }
     
+    // Add cache-busting headers
+    config.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+    config.headers['Pragma'] = 'no-cache';
+    config.headers['Expires'] = '0';
+    
     return config;
   },
   (error) => {
